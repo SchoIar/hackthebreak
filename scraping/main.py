150293fb8dbecf94bb50a1cea8745c9cc4c9588f
Anton <antonilic7@gmail.com>
2023-03-11 10:21:41 -0800

    reading and writing data

--- scraping/main.py
+++ scraping/main.py
@@ -9,5 +9,13 @@ def read_json(filename='jobs.json'):
     with open(filename, 'r') as file:
         return json.load(file)
 
+def write_json(data,filename='jobs.json'):
+    with open(filename, 'w') as file:
+        compiledData = read_json()
+        compiledData["job-list"].append((data))
+        json.dump(compiledData, file)
 
+
+write_json()
 print(read_json())
+
