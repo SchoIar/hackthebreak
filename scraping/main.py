81b1c90df5c02a3a438d589e369cfff2002ea117
Anton <antonilic7@gmail.com>
2023-03-11 09:49:30 -0800

    reading json files

---
+++ scraping/main.py
@@ -0,0 +1,12 @@
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+Password = os.getenv('PASSWORD')
+Email = os.getenv('EMAIL')
+
+def read_json(filename='jobs.json'):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+print(read_json('jobs.json'))
