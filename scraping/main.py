3774fe33afa14ac46219512cceb8441e2769d166
Anton <antonilic7@gmail.com>
2023-03-11 20:42:41 -0800

    added OOP

--- scraping/main.py
+++ scraping/main.py
@@ -6,51 +6,56 @@ from linkedin_api import Linkedin
 from dotenv import load_dotenv
 import sys
 
+class Scrape():
+    def __init__(self, api):
+        self.api = api
+        
+    def read_json(self, filename='jobs.json'):
+        with open(filename, 'r') as file:
+            return json.load(file)
 
-load_dotenv()
-Password = os.getenv('PASSWORD')
-Email = os.getenv('EMAIL')
+    def write_json(self, newData):
+        with open('jobs.json', 'r+') as file:
+            data = self.read_json()
+            data['job-list'].append(newData)
+            json.dump(data, file)
 
-api = Linkedin(Email, Password)
+    def searchJobs(self, apiChosen, numberOfSearches, keywordChosen, offsetNumber):
+        jobs = apiChosen.search_jobs(keywordChosen, remote = 1, limit = \
+                            numberOfSearches, offset = offsetNumber)
+        for job in jobs:
+            title = job['title']
+            jobID = job['dashEntityUrn'].split(':')[-1] 
+            location = job['formattedLocation']
+            #jobDetails = api.get_job(jobID)
+            jobLink = f'https://www.linkedin.com/jobs/view/{jobID}/'
+            job = {
+                "Job title":title,
+                "Job link":jobLink,
+                "Location":location
+            }
+            print(f"{title} : {jobID} : {location}")
+            self.write_json(job)
+        
 
-data = {"job-list": [{}]}
-with open('jobs.json', 'w') as file:
-    json.dump(data, file)
-
-def read_json(filename='jobs.json'):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-def write_json(newData):
-    with open('jobs.json', 'r+') as file:
-        data = read_json()
-        data['job-list'].append(newData)
+    def findSWEJobs(self, apiChosen):
+        listOfJobs = ["Software Developer","Software Engineer", "Software Intern","SDET","Developer Intern","Software co-op","Junior Developer"] 
+        for i in range(11,101):
+            
+            for element in listOfJobs:
+                self.searchJobs(apiChosen, 1, element, i)
+                time.sleep(1*random.randint(1,5)+2)
+    
+if(__name__ == "__main__"):
+    load_dotenv()
+    Password = os.getenv('PASSWORD')
+    Email = os.getenv('EMAIL')
+    api = Linkedin(Email, Password)
+    data = {"job-list": [{}]}
+    with open('jobs.json', 'w') as file:
         json.dump(data, file)
 
-def searchJobs(numberOfSearches, keywordChosen, offsetNumber):
-    jobs = api.search_jobs(keywordChosen, remote = 1, limit = \
-                           numberOfSearches, offset = offsetNumber)
-    for job in jobs:
-        title = job['title']
-        jobID = job['dashEntityUrn'].split(':')[-1] 
-        location = job['formattedLocation']
-        #jobDetails = api.get_job(jobID)
-        jobLink = f'https://www.linkedin.com/jobs/view/{jobID}/'
-        job = {
-            "Job title":title,
-            "Job link":jobLink,
-            "Location":location
-        }
-        print(f"{title} : {jobID} : {location}")
-        write_json(job)
-    return True
-
-listOfJobs = ["Software Developer","Software Engineer", "Software Intern","SDET","Developer Intern","Software co-op","Junior Developer"] 
-for i in range(11,101):
+    scraper = Scrape(api)
+    scraper.findSWEJobs(api)
     
-    for element in listOfJobs:
-        searchJobs(1, element, i)
-        print('Done')
-        time.sleep(1*random.randint(1,5)+2)
-   
+    
