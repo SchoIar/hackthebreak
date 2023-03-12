7d094531734b06fffcbeecc012affafdd53dafc7
Anton <antonilic7@gmail.com>
2023-03-11 19:40:44 -0800

    edited for SQL integration

--- db/main.py
+++ db/main.py
@@ -4,30 +4,14 @@ import time
 import random
 from linkedin_api import Linkedin
 from dotenv import load_dotenv
-import sys
+from SQLManager import SQLManager
 
-sys.path.insert
 
 load_dotenv()
 Password = os.getenv('PASSWORD')
 Email = os.getenv('EMAIL')
-
 api = Linkedin(Email, Password)
-
-data = {"job-list": [{}]}
-with open('jobs.json', 'w') as file:
-    json.dump(data, file)
-
-def read_json(filename='jobs.json'):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-
-def write_json(newData):
-    with open('jobs.json', 'r+') as file:
-        data = read_json()
-        data['job-list'].append(newData)
-        json.dump(data, file)
+sql = SQLManager()
 
 def searchJobs(numberOfSearches, keywordChosen, offsetNumber):
     jobs = api.search_jobs(keywordChosen, remote = 1, limit = \
@@ -36,15 +20,10 @@ def searchJobs(numberOfSearches, keywordChosen, offsetNumber):
         title = job['title']
         jobID = job['dashEntityUrn'].split(':')[-1] 
         location = job['formattedLocation']
-        #jobDetails = api.get_job(jobID)
         jobLink = f'https://www.linkedin.com/jobs/view/{jobID}/'
-        job = {
-            "Job title":title,
-            "Job link":jobLink,
-            "Location":location
-        }
-        print(f"{title} : {jobID} : {location}")
-        write_json(job)
+
+        sql.newJob(str(jobID), jobLink, title, location)
+        
     return True
 
 for i in range(11,101):  
