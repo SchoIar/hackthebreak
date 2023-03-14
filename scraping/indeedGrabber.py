2c014e06af9cd8a3c5ae6e09fb51a451a1d7c021
Anton <antonilic7@gmail.com>
2023-03-13 21:56:52 -0700

    stable version

--- scraping/indeedGrabber.py
+++ scraping/indeedGrabber.py
@@ -6,6 +6,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
 #driver = webdriver.Chrome('./chromedriver')
 driver.get("https://ca.indeed.com/?r=us")
 elem = driver.find_element(By.NAME, "q")
@@ -19,12 +20,18 @@ time.sleep(5)
 location.send_keys(Keys.RETURN)
 time.sleep(5)
 #TODO: Find button via xpath
-driver.find_element(By.CSS_SELECTOR,"#filter-dateposted")
+
+"""driver.find_element(By.CSS_SELECTOR,"#filter-dateposted")
 time.sleep(5)
 driver.find_element(By.CSS_SELECTOR,"label.css-irr45f:nth-child(2)")#label.css-irr45f:nth-child(2)
 time.sleep(5)
 #.css-4of6ml
 driver.find_element(By.CSS_SELECTOR,".css-4of6ml")
+time.sleep(5)"""
+element = driver.find_element(By.XPATH,'/html/body/main/div/div[1]/div/div/div[2]/div/div/div/div[2]/div/div[1]')#button id="filter-dateposted"
+element.click()
 time.sleep(5)
+within24hours = driver.find_element(By.XPATH, '/html/body/main/div/div[1]/div/div/div[2]/div/div/div/div[2]/div/div[1]/ul/li[1]/a')
+within24hours.click()
 time.sleep(5)
 driver.close()
