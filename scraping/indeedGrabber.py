78c71b43737a2a30a48524ae13d18882f02cab8d
Anton <antonilic7@gmail.com>
2023-03-14 20:34:55 -0700

    Indeed scrapper

--- scraping/indeedGrabber.py
+++ scraping/indeedGrabber.py
@@ -10,12 +10,13 @@ from selenium.webdriver.chrome.options import Options
 
 
 def getJob(nameOfJob):
+    '''Scrapes jobs from Indeed returning a list with their names.'''
+
     nameOfJob = "Software Developer"
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    #chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-    #driver = webdriver.Chrome('./chromedriver')
     driver.get("https://ca.indeed.com/?r=us")
     elem = driver.find_element(By.NAME, "q")
     elem.clear()
@@ -27,15 +28,7 @@ def getJob(nameOfJob):
     time.sleep(5)
     location.send_keys(Keys.RETURN)
     time.sleep(5)
-    #TODO: Find button via xpath
 
-    """driver.find_element(By.CSS_SELECTOR,"#filter-dateposted")
-    time.sleep(5)
-    driver.find_element(By.CSS_SELECTOR,"label.css-irr45f:nth-child(2)")#label.css-irr45f:nth-child(2)
-    time.sleep(5)
-    #.css-4of6ml
-    driver.find_element(By.CSS_SELECTOR,".css-4of6ml")
-    time.sleep(5)"""
     element = driver.find_element(By.XPATH,'/html/body/main/div/div[1]/div/div/div[2]/div/div/div/div[2]/div/div[1]')#button id="filter-dateposted"
     element.click()
     time.sleep(5)
@@ -43,13 +36,16 @@ def getJob(nameOfJob):
     within24hours.click()
     time.sleep(5)
     currentUrl = driver.current_url
+    print(currentUrl)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    jobs = soup.find_all("td",{"class":"resultContent"})
+    for job in jobs:
+        name = job.find("span")['title']
+        location = job.find("span",{"class":"companyName"}).string
+        print(f'{name} at {location}')#{link}')
     time.sleep(5)
     #content = driver.find_elements(By.CLASS_NAME, 'resultContent')
     
     driver.close()
-
-
-
-#get all jobs stored at class = resultContent
 
 getJob('Software Developer')
