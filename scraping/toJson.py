dc14878f200921c0d13a9f22ca56b2a0727d94f7
Anton <antonilic7@gmail.com>
2023-03-15 21:35:44 -0700

    added reset

--- scraping/toJson.py
+++ scraping/toJson.py
@@ -4,16 +4,15 @@ import time
 import random
 
 class toJson():
-
-    def __init__(self, selectedField, filename):
+    """Appends selected Fieldname to specified filename, under selected field """
+    def __init__(self, selectedField, filename, fieldname):
         self.selectedFields = selectedField
         self.filename = filename
-        ''' data = {"job-list": [{}]}
-        with open(filename, 'w') as file:
-            json.dump(data, file)'''
+        self.fieldname = fieldname
+        
     
     def reset_file(self):
-        data = {"job-list": [{}]}
+        data = {"job-list": []}
         with open(self.filename, 'w') as file:
             json.dump(data, file)
         
@@ -24,9 +23,10 @@ class toJson():
     def write_json(self):
         with open(self.filename, 'r+') as file:
             data = self.read_json()
-            data['job-list'].append(self.selectedFields)
+            data[self.fieldname].append(self.selectedFields)
             file.seek(0)
-            json.dump(data, file, indent = 2)
+            json.dump(data, file, indent = 3)
 
 if(__name__ == "__main__"):
-    toJson(selectedField={'A':'B'},filename='jobs.json').write_json()
+    #toJson(selectedField={'A':'B'},filename='jobs.json',fieldname='job-list').reset_file() #clear file
+    toJson(selectedField={'A':'B'},filename='jobs.json',fieldname='job-list').write_json()
